45c70cafdbbf37f4f35294af6c0ac13f7e5edeb8
Dan Nop <daniel.nop@liveperson.com>
2022-10-07 14:26:24 -0400

    data

--- dynamicOpeners.js
+++ dynamicOpeners.js
@@ -34,7 +34,7 @@ lpTag.external.dynamicOpeners = {
             }
             
             defineContainer().then(function(data) {
-                console.log("CONTAINER", data.children[0]);
+                console.log("CONTAINER", data);
                 // get the current opener text node
                 let nodeList = data?.children[0]?.children[0]?.childNodes;
                 console.log(nodeList);
