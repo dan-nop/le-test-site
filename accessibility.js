02f4ec9deff8f63583481d03d9037d0aa9f4e589
Dan Nop <daniel.nop@liveperson.com>
2022-11-09 13:12:38 -0500

    z index

--- accessibility.js
+++ accessibility.js
@@ -70,16 +70,19 @@ lpTag.external.updateZIndex = {
         // console.log("DATA", data);
         console.log("ENG. EngType", data.msg.engagementType);
         if (data.msg.engagementType === 6) {
-            setTimeout (update)
-            let stickyBtn = document.querySelector('div[id^="LPMcontainer"][role="button"]');
-            console.log(stickyBtn);
-            if (stickyBtn === null) {
-            return;
-            } else {
-            stickyBtn.style.zIndex = "100000";
+            setTimeout (updateZIndex, 5);
+            function updateZIndex() {
+                let stickyBtn = document.querySelector('div[id^="LPMcontainer"][role="button"]');
+                console.log(stickyBtn);
+                if (stickyBtn === null) {
+                updateZIndex();
+                } else {
+                stickyBtn.style.zIndex = "100000";
+                }
             }
+         
         } else {
-            return;
+            console.log("no sticky found");
         }
         
     }
