09bfec1b48cd1da8aa9872e5279e1342d101809c
Dan Nop <dnop@liveperson.com>
2023-04-10 13:41:11 -0400

    changed web tag

--- accessibility.js
+++ accessibility.js
@@ -5,24 +5,8 @@ lpTag.external.accessibilityFix = {
     // handle the offer_impression event
     engagementRenderedHandler: function (data) {      
         try {
-            const options = {
-                method: "GET",
-                headers: {
-                    "Accept": "application/json",
-                    "Access-Control-Allow-Origin": "*",
-                    "Content-Type": "application/json",
-                    "Authorization": `OAuth,
-                    oauth_consumer_key="0685bd9184jfhq22",
-                    oauth_token="ad180jjd733klru7",
-                    oauth_signature_method="HMAC-SHA1",
-                    oauth_signature="wOJIO9A2W5mFwDgiDvZbTSMK%2FPY%3D",
-                    oauth_timestamp="137131200",
-                    oauth_nonce="4572616e48616d6d65724c61686176",
-                    oauth_version="1.0"`
-                }
-            };
             
-            fetch("https://va.v.liveperson.net/api/account/23979466/monitoring/visitors/QzZWMwZmY5MjI4YjQzNjJk/visits/current/state?v=1&filter=agent&sid=zUbA0zY3R-Sx7pars-tTgA", options )
+            window.fetch("https://va.v.liveperson.net/api/account/23979466/monitoring/visitors/QzZWMwZmY5MjI4YjQzNjJk/visits/current/state?v=1&filter=agent&sid=zUbA0zY3R-Sx7pars-tTgA", {headers: "GET", "Accept": "application/json", "Content-Type": "application/json", "Authorization": `OAuth oauth_signature=""`} )
               .then((response) => response.json())
               .then((data) => console.log(data));
             // is this an embedded button (engagementType 5) and an HTML engagement (renderingType 1)
