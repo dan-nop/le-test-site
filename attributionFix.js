9d2b60e6a69d1c16e873dd3b82cfa08300bafb9e
Dan Nop <dnop@liveperson.com>
2023-05-12 13:28:47 -0400

    sticky but

--- attributionFix.js
+++ attributionFix.js
@@ -60,7 +60,7 @@ lpTag.external.accessibilityFix = {
 				else if (data.engagementType === 6 && data.renderingType === 0) {
 				//target sticky parent container and update z-index to be lower than the overlay parent container 
 				let stickyBtn = document.querySelector('div[id^="LPMcontainer"][role="button"]');
-				// console.log(stickyBtn);
+				console.log("stickyBtn", stickyBtn);
 				stickyBtn.setAtribute('style', 'z-index:995!important');
 			}            
         } catch (e) {
