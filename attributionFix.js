a055890243a524317a0050497a330bccaa7fa71a
Dan Nop <dnop@liveperson.com>
2023-05-12 14:42:22 -0400

    z important

--- attributionFix.js
+++ attributionFix.js
@@ -63,7 +63,7 @@ lpTag.external.accessibilityFix = {
 				let stickyBtn = document.querySelector('div[id^="LPMcontainer"][role="button"]');
 				// console.log(stickyBtn);
 				console.log("stickyBtn", stickyBtn);
-                stickyBtn.style.zIndex = 995;
+                stickyBtn.style.setProperty('z-index', '995', 'important');
 			}            
         } catch (e) {
             console.error();
